fb74f440b64b4c5bf542827fbe3ffc6b0b01b625
Ahaif <haifouli.abdelmoughite@gmail.com>
2023-11-26 14:51:57 +0200

    add tests

--- module01/ex02/test.py
+++ module01/ex02/test.py
@@ -18,6 +18,87 @@ class TestVectorAddition(unittest.TestCase):
         
         with self.assertRaises(ValueError):
             result = v1 + v2
+    
+    def test_radd(self):
+        v1 = Vector([[1.0], [2.0], [3.0]])
+        result = 4.0 + v1
+        expected_values = [[5.0], [6.0], [7.0]]
+        expected_shape = (3, 1)
+
+        self.assertEqual(result.values, expected_values)
+        self.assertEqual(result.shape, expected_shape)
+    
+    def test_subtraction_same_shape(self):
+        v1 = Vector([[1.0], [2.0], [3.0]])
+        v2 = Vector([[4.0], [5.0], [6.0]])
+        result = v1 - v2
+        expected_values = [[-3.0], [-3.0], [-3.0]]
+        expected_shape = (3, 1)
+
+        self.assertEqual(result.values, expected_values)
+        self.assertEqual(result.shape, expected_shape)
+
+    def test_subtraction_different_shape(self):
+        v1 = Vector([[1.0], [2.0], [3.0]])
+        v2 = Vector([[4.0], [5.0]])
+        
+        with self.assertRaises(ValueError):
+            result = v1 - v2
+
+    def test_rsub(self):
+        v1 = Vector([[1.0], [2.0], [3.0]])
+        result = 4.0 - v1
+        expected_values = [[3.0], [2.0], [1.0]]
+        expected_shape = (3, 1)
+
+        self.assertEqual(result.values, expected_values)
+        self.assertEqual(result.shape, expected_shape)
+    
+    def test_multiplication(self):
+        v1 = Vector([[1.0], [2.0], [3.0]])
+        result = v1 * 3
+        expected_values = [[3.0], [6.0], [9.0]]
+        expected_shape = (3, 1)
+
+        self.assertEqual(result.values, expected_values)
+        self.assertEqual(result.shape, expected_shape)
+    
+    def test_rmul(self):
+        v1 = Vector([[1.0], [2.0], [3.0]])
+        result = 3 * v1
+        expected_values = [[3.0], [6.0], [9.0]]
+        expected_shape = (3, 1)
+
+        self.assertEqual(result.values, expected_values)
+        self.assertEqual(result.shape, expected_shape)
+
+    def test_division(self):
+        v1 = Vector([[1.0], [2.0], [3.0]])
+        result = v1 / 3
+        expected_values = [[1/3], [2/3], [3/3]]
+        expected_shape = (3, 1)
+
+        self.assertEqual(result.values, expected_values)
+        self.assertEqual(result.shape, expected_shape)
+    
+    def test_rdiv(self):
+        v1 = Vector([[1.0], [2.0], [3.0]])
+        result = 3 / v1
+        expected_values = [[3.0], [1.5], [1.0]]
+        expected_shape = (3, 1)
+
+        self.assertEqual(result.values, expected_values)
+        self.assertEqual(result.shape, expected_shape)
+    
+    def test_dot(self):
+        v1 = Vector([[1.0], [2.0], [3.0]])
+        v2 = Vector([[4.0], [5.0], [6.0]])
+        result = v1.dot(v2)
+        expected = 32.0
+
+        self.assertEqual(result, expected)
+    
+  
 
 if __name__ == '__main__':
     unittest.main()
--- module01/ex02/vector.py
+++ module01/ex02/vector.py
@@ -29,6 +29,85 @@ class Vector:
             result_values.append([x[0] + y[0]])
 
         return Vector(result_values)
+    
+    def __radd__(self, other):
+        if isinstance(other, (int, float)):
+            result_values = [[x[0] + other] for x in self.values]
+            return Vector(result_values)
+        else:
+            raise ValueError("Invalid operation: Cannot perform addition with non-scalar types")
+
+
+    def __sub__(self, other):
+        if self.shape != other.shape:
+            raise ValueError("Vectors must have the same shape for addition")
+
+        result_values = []
+        for x, y in zip(self.values, other.values):
+            result_values.append([x[0] - y[0]])
+        return Vector(result_values)
+
+    def __rsub__(self, other):
+        if isinstance(other, (int, float)):
+            result_values = [[other - x[0]] for x in self.values]
+            return Vector(result_values)
+        else:
+            raise ValueError("Invalid operation: Cannot perform addition with non-scalar types")
+
+
+    def __mul__(self, scalar):
+        result_values = [[x[0] * scalar] for x in self.values]
+        return Vector(result_values)
+
+    def __rmul__(self, scalar):
+        return self.__mul__(scalar)
+
+    def __truediv__(self, scalar):
+        if scalar == 0:
+            raise ZeroDivisionError("Division by zero is not allowed")
+        result_values = [[x[0] / scalar] for x in self.values]
+        return Vector(result_values)
+
+    def __rtruediv__(self, scalar):
+        if isinstance(scalar, (int, float)):
+            if 0 in (x[0] for x in self.values):
+                raise ZeroDivisionError("Division by zero is not allowed")
+            result_values = [[scalar / x[0]] for x in self.values]
+            return Vector(result_values)
+        else:
+            raise ValueError("Invalid operation: Cannot perform division with non-scalar types")
+
+
+    def __repr__(self):
+        return str(self.values)
+
+    def __str__(self):
+        return str(self.values)
+
+    def dot(self, other):
+        if self.shape != other.shape:
+            raise ValueError("Vectors must have the same shape for dot product")
+        result = sum(x[0] * y[0] for x, y in zip(self.values, other.values))
+        return result
+
+    def T(self):
+        result_values = [[x for x in row] for row in zip(*self.values)]
+        if self.shape == (1, len(self.values[0])):
+            return Vector(result_values)
+        elif self.shape == (len(self.values), 1):
+            return Vector(result_values).reshape((1, len(self.values)))
+
+    def test_column_vector_transpose(self):
+        v = Vector([[1], [2], [3]])
+        result = v.T()
+        expected_values = [[1, 2, 3]]
+        self.assertEqual(result.values, expected_values)
+
+    def test_row_vector_transpose(self):
+        v = Vector([[1, 2, 3]])
+        result = v.T()
+        expected_values = [[1], [2], [3]]
+        self.assertEqual(result.values, expected_values)
 
 
 
